a47d52fbe36d577e1016fdfd85428b80249b5e68
anhkhoa2001 <khoadamtam@gmail.com>
2022-12-22 14:26:32 +0700

    cap nhat file json postman

---
+++ src/main/java/com/example/restlibrary/model/Customer.java
@@ -0,0 +1,53 @@
+package com.example.restlibrary.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rest_customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "status")
+    private Boolean status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
