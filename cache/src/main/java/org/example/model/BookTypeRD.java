ea4217b397233e8c57e0605f56465ff5b71b76cf
anhkhoa2001 <khoadamtam@gmail.com>
2022-12-28 15:01:32 +0700

    update

--- cache/src/main/java/org/example/model/BookTypeRD.java
+++ cache/src/main/java/org/example/model/BookTypeRD.java
@@ -3,11 +3,13 @@ package org.example.model;
 import lombok.Data;
 import org.springframework.data.redis.core.RedisHash;
 
+import javax.persistence.Id;
 import java.util.Set;
 
 @Data
 @RedisHash("BookTypeRD")
 public class BookTypeRD {
+    @Id
     private Integer type_id;
 
     private String name;
