e6c1b43fb2cc33b13a36f57f1edb681bdc97603a
anhkhoa2001 <khoadamtam@gmail.com>
2023-01-04 13:41:40 +0700

    update

--- api/src/main/java/org/example/Main.java
+++ api/src/main/java/org/example/Main.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 public class Main {
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
     }
